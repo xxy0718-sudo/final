9ac51a95234f727fcecc2de82b97bc814bc0a510
1 <xxy0718@g.skku.edu>
2026-05-12 20:32:39 +0900

    Create visual_style.py

---
+++ visual_style.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+def visual_style_summary():
+    st.subheader("📝 Visual Style Summary")
+
+    st.write(
+        "Warm tones are commonly used in romance and emotional films, while dark tones are frequently seen in thriller and drama genres. Cool tones are often used in science fiction and action movies to create futuristic atmospheres."
+    )
